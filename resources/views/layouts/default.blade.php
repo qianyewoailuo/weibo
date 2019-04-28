d30eb3aa23b6e9c20dec0ca326aac920987682f6
qianyewoailuo <qianyewoailuo@126.com>
2019-04-28 17:45:58 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,10 +3,27 @@
 
 <head>
   <title>@yield('title','Weibo APP') - Laravel入门</title>
+  <!-- <link rel="stylesheet" href="/css/app.css"> -->
+  <link rel="stylesheet" href="/css/app.css">
+  <!-- Laravel是以public文件夹为根目录的，因此我们可以使用下面这行代码来为Laravel引入样式 -->
+  <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <!-- justify-content-end 末尾定位 -->
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 
 </html>
