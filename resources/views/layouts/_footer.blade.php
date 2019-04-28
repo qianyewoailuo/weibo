6ce9b721b158c5e9de9dadc71258f656d1836d81
qianyewoailuo <qianyewoailuo@126.com>
2019-04-28 18:38:23 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <img src="https://iocaffcdn.phphub.org//uploads/communities/WtC3cPLHzMbKRSZnagU9.png" alt="laravel-china" class="brand-icon">
+  <a href=" https://laravel-china.org/courses" target=_blank >
+    刻意练习，每日精进
+  </a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
