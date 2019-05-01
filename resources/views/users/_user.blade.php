f8b2fa4473c496ea44262a86963d48753e5900e0
qianyewoailuo <qianyewoailuo@126.com>
2019-05-01 18:33:46 +0000

    最后一次修改用户列表

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,15 +1,14 @@
 <div class="list-group-item">
-  <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
-  <a href="{{ route('users.show', $user) }}">
-    {{ $user->name }}
-  </a>
-
-  <!-- 模板中使用删除策略授权 -->
-  @can('destroy',$user)
-  <form action="{{route('users.destroy',$user->id)}}" method="post" class="float-right">
-    {{csrf_field()}}
-    {{method_field('DELETE')}}
-    <button type="submit" class="btn btn-sm badge-danger delete-btn">删除</button>
-  </form>
-  @endcan
+    <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
+    <a href="{{ route('users.show', $user) }}">
+        {{ $user->name }}
+    </a>
+    <!-- 模板中使用删除授权策略 -->
+    @can('destroy', $user)
+    <form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+        <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+    </form>
+    @endcan
 </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,17 +1,18 @@
-@extends('layouts.default');
-@section('title','所有用户');
+@extends('layouts.default')
+@section('title', '所有用户')
 
 @section('content')
 <div class="offset-md-2 col-md-8">
-  <h2 class="mb-4 text-center">所有用户</h2>
-  <div class="list-group list-group-flush">
-    @foreach ($users as $user)
-    <!-- 局部视图细分 -->
-      @include('users._user')
-    @endforeach
-    <!-- <div class="mt-3">
-      {!! $users->render() !!}
-    </div> -->
-  </div>
+    <h2 class="mb-4 text-center">所有用户</h2>
+    <!-- 用户列表 -->
+    <div class="list-group list-group-flush">
+        @foreach ($users as $user)
+        @include('users._user')
+        @endforeach
+    </div>
+    <!-- 分页 -->
+    <div class="mt-3">
+        {!! $users->render() !!}
+    </div>
 </div>
 @stop
