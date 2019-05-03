ebae88cf51b77159a93d47591daaf22e80884be7
qianyewoailuo <qianyewoailuo@126.com>
2019-05-03 05:07:35 +0000

    lang服务注册

--- config/app.php
+++ config/app.php
@@ -158,7 +158,9 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        // 语言包服务替换
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
+        // Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
